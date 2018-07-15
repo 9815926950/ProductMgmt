c110c29f1b836cab87b63a78e5fa2affea91d3f9
ramuale121@gmail.com <ramuale121@gmail.com>
2018-07-15 07:35:17 +0545

    Project upload first time

---
+++ Data/Migrations/20180713053130_Product table from Products class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductMgmt.Data.Migrations
+{
+    public partial class ProducttablefromProductsclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
